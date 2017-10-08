419152e584953b76c369cb06bbaa03e471049697
TechnicallyRachel <paintmetothemoon@gmail.com>
2017-10-08 16:12:54 -0700

    Update TDRamsBot.py

--- TDRamsBot.py
+++ TDRamsBot.py
@@ -3,11 +3,9 @@
 # This bot will retweet every 5 minutes, 
 # using your search query for the 5 newest tweets.
 
-# If you haven't changed credentials.py yet with your own Twitter
-# account settings, this script will tweet at twitter.com/lacunybot
-
 # Setting up bot
 # credentials.py contains twitter access app keys
+# make sure twython and tweepy are installed
 import tweepy, time 
 from twython import Twython, TwythonError
 from credentials import *
@@ -23,10 +21,12 @@ filter = " OR ".join(good_words)
 blacklist = " -".join(naughty_words)
 keywords = filter + blacklist
 
-# harnessing twitter's search query
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
-search_results = twitter.search(q=keywords, count=10)
+# harnessing twitter's search query
+# decide how many times you wish to retweet per cycle. 
+# As you see I put '5'.
+search_results = twitter.search(q=keywords, count=5)
 try:
    for tweet in search_results["statuses"]:
        try:
